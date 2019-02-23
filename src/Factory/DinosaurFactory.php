781982f3e111d6d0d845858c6b39762b1061ac53
petrero <petrero@gmail.com>
2019-02-23 20:53:07 +0200

    Implement the Code
    Step 2: make this work with as little work as possible... or at least without over-engineering it. We can cheat: copy the code from the old growVelociraptor() method. I'll keep this method here just as an example. Back in DinosaurFactory, paste, change the new static to new Dinosaur, change the argument to int $ length and give this a Dinosaur return type.
    Try it out:
    ./vendor/bin/phpspec run
    Green!

--- src/Factory/DinosaurFactory.php
+++ src/Factory/DinosaurFactory.php
@@ -2,8 +2,13 @@
 
 namespace App\Factory;
 
+use App\Entity\Dinosaur;
+
 class DinosaurFactory {
-  public function growVelociraptor($argument1) {
-    // TODO: write logic here
+  public function growVelociraptor(int $length): Dinosaur {
+	  $dinosaur = new Dinosaur('velociraptor', true);
+	  $dinosaur->setLength(5);
+
+	  return $dinosaur;
   }
 }
