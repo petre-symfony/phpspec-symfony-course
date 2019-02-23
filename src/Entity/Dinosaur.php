2f9636a6748a2dae9b9f32e64610d8103008bf88
petrero <petrero@gmail.com>
2019-02-23 19:46:49 +0200

    Copy code from previous branch

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+class Dinosaur {
+	private $length=0;
+	/**
+	 * @var string
+	 */
+	private $genus;
+	/**
+	 * @var bool
+	 */
+	private $isCarnivorous;
+
+	public function __construct(string $genus='Unknown', bool $isCarnivorous=false) {
+		$this->genus = $genus;
+		$this->isCarnivorous = $isCarnivorous;
+	}
+
+  public function getLength():int {
+    return $this->length;
+  }
+
+  public function setLength(int $length) {
+    $this->length = $length;
+  }
+
+  public function getDescription(): string {
+    return sprintf(
+      "The %s %scarnivorous dinosaur is %d meters long",
+	    $this->genus,
+	    $this->isCarnivorous ? '' : 'non-',
+		    $this->length
+    );
+  }
+}
