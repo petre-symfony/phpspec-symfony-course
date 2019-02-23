58ede163adb9ab4be4e8e03aefc7f0e1e38096eb
petrero <petrero@gmail.com>
2019-02-23 12:51:47 +0200

    Copy code from previous branch

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+class Dinosaur {
+	private $length=0;
+
+  public function __construct($argument1, $argument2) {
+    // TODO: write logic here
+  }
+
+  public function getLength():int {
+    return $this->length;
+  }
+
+  public function setLength(int $length) {
+    $this->length = $length;
+  }
+
+  public function getDescription(): string {
+    return 'The Unknown non-carnivorous dinosaur is 0 meters long';
+  }
+}
