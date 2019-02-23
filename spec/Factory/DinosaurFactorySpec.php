df018d4e9fbaf3370eb9f50853c20be2c8c5a1d9
petrero <petrero@gmail.com>
2019-02-23 20:32:11 +0200

    Run: ./vendor/bin/phpspec describe App/Factory/DinosaurFactory
    Describing a new Class
    So, hey! We need a new class! Well, to say it better, it's time for us to describe a new class: ./vendor/bin/phpspec describe and, for the name, how about App/Factory/DinosaurFactory.
    That creates one new file: DinosaurFactorySpec. Let's go check it out! Like last time, we get one super basic example for free - asserting that  should be an instance of DinosaurFactory. That's... kinda silly... but it's enough to force some code generation!

---
+++ spec/Factory/DinosaurFactorySpec.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace spec\App\Factory;
+
+use App\Factory\DinosaurFactory;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class DinosaurFactorySpec extends ObjectBehavior {
+  function it_is_initializable() {
+    $this->shouldHaveType(DinosaurFactory::class);
+  }
+}
