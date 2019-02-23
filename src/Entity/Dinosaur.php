1b0b0aa05dfa6932263fe478438068466cd0abde
petrero <petrero@gmail.com>
2019-02-23 09:56:05 +0200

    Copy code from previous branch

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+class Dinosaur {
+	private $length=0;
+
+  public function getLength():int {
+    return $this->length;
+  }
+
+    public function setLength(int $length) {
+      $this->length = $length;
+    }
+}
