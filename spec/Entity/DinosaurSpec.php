bdde1e9d12c682581c52b5662e8eba0a240e5373
petrero <petrero@gmail.com>
2019-02-23 11:20:51 +0200

    Copy code from previous branch

---
+++ spec/Entity/DinosaurSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\App\Entity;
+
+use App\Entity\Dinosaur;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use PhpSpec\Exception\Example\FailureException;
+
+class DinosaurSpec extends ObjectBehavior {
+	public function getMatchers(): array {
+		return [
+			'returnZero' => function($subject){
+				if ($subject !== 0){
+					throw new FailureException(sprintf(
+						"Returned value should be zero, got %s",
+							$subject
+					));
+				}
+				return true;
+			}
+		];
+	}
+
+	function it_is_initializable() {
+      $this->shouldHaveType(Dinosaur::class);
+  }
+
+  function it_should_default_to_zero_length(){
+  	$this->getLength()->shouldReturn(0);
+  }
+
+  function it_should_default_to_zero_length_using_custom_matcher(){
+    $this->getLength()->shouldReturnZero();
+  }
+
+  function it_should_allow_to_set_length(){
+  	$this->setLength(9);
+
+  	$this->getLength()->shouldReturn(9);
+  }
+
+  function it_should_not_shrink(){
+		$this->setLength(15);
+
+		$this->getLength()->shouldBeGreaterThan(12);
+  }
+}
