9eb5619b4bcc035e58823556cd9fa0e96d259c00
petrero <petrero@gmail.com>
2019-02-24 13:08:25 +0200

    Copy code from previous branch

---
+++ src/Entity/Enclosure.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+use App\Exception\NotABuffetException;
+
+class Enclosure {
+	/** @var Dinosaur[] */
+	private $dinosaurs = [];
+
+  public function getDinosaurs():array {
+    return $this->dinosaurs;
+  }
+
+  public function addDinosaur(Dinosaur $dinosaur) {
+  	if(!$this->canAddDinosaur($dinosaur)){
+  		throw new NotABuffetException();
+	  }
+    $this->dinosaurs[] = $dinosaur;
+  }
+
+	private function canAddDinosaur(Dinosaur $dinosaur):bool {
+    return count($this->dinosaurs) === 0
+	    || ($this->dinosaurs[0]->isCarnivorous() === $dinosaur->isCarnivorous());
+	}
+
+}
