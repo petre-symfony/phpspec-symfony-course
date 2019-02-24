4e0184711ffe095dc59c43ad1a9127b336e27a30
petrero <petrero@gmail.com>
2019-02-24 15:23:49 +0200

    Copy code from previous branch

---
+++ src/Entity/Enclosure.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+use App\Exception\DinosaursAreRunningRampantException;
+use App\Exception\NotABuffetException;
+
+class Enclosure {
+	/** @var Dinosaur[] */
+	private $dinosaurs = [];
+
+	/** @var Security[] */
+	private $securities=[];
+
+  public function getDinosaurs():array {
+    return $this->dinosaurs;
+  }
+
+  public function addDinosaur(Dinosaur $dinosaur) {
+  	if(!$this->isSecurityActive()){
+  		throw new DinosaursAreRunningRampantException('Are you crazy?!?');
+	  }
+  	if(!$this->canAddDinosaur($dinosaur)){
+  		throw new NotABuffetException();
+	  }
+    $this->dinosaurs[] = $dinosaur;
+  }
+
+	private function canAddDinosaur(Dinosaur $dinosaur):bool {
+    return count($this->dinosaurs) === 0
+	    || $dinosaur->hasSameDietAs($this->dinosaurs[0]);
+	}
+
+	private function isSecurityActive(): bool {
+  	foreach ($this->securities as $security){
+  		if ($security->getIsActive()){
+  			return true;
+		  }
+	  }
+
+  	return false;
+	}
+
+}
