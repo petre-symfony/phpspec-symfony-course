e2e77e7dd8eb046fa401752b3f38b9ffbe175ed4
petrero <petrero@gmail.com>
2019-02-24 19:47:41 +0200

    Copy code from previous branch

---
+++ spec/Service/EnclosureBuilderServiceSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\App\Service;
+
+use App\Factory\DinosaurFactory;
+use App\Service\EnclosureBuilderService;
+use PhpSpec\ObjectBehavior;
+use App\Entity\Enclosure;
+
+class EnclosureBuilderServiceSpec extends ObjectBehavior {
+  function it_is_initializable() {
+    $this->shouldHaveType(EnclosureBuilderService::class);
+  }
+
+	function it_builds_enclosure_with_dinosaurs(DinosaurFactory $dinosaurFactory){
+  	$this->beConstructedWith($dinosaurFactory);
+		$enclosure = $this->buildEnclosure(1, 2);
+
+		$enclosure->shouldBeAnInstanceOf(Enclosure::class);
+		$enclosure->isSecurityActive()->shouldReturn(true);
+
+		var_dump($enclosure->getDinosaurs());
+	}
+}
---
+++ src/Service/EnclosureBuilderService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Entity\Enclosure;
+use App\Entity\Security;
+use App\Factory\DinosaurFactory;
+
+class EnclosureBuilderService {
+
+  public function __construct(DinosaurFactory $dinosaurFactory) {
+    // TODO: write logic here;
+  }
+
+	public function buildEnclosure(
+			int $numberOfSecuritySystems = 1,
+			int $numberOfDinosaurs = 3
+	): Enclosure {
+		$enclosure = new Enclosure();
+
+		$this->addSecuritySystems($numberOfSecuritySystems, $enclosure);
+
+		return $enclosure;
+	}
+
+	private function addSecuritySystems(
+		int $numberOfSecuritySystems,
+		Enclosure $enclosure
+	){
+		for($i=0; $i<$numberOfSecuritySystems; $i++){
+			$securityName = array_rand(['Fence', 'Electric fence', 'Guard tower']);
+			$security = new Security($securityName, true, $enclosure);
+
+			$enclosure->addSecurity($security);
+		}
+	}
+}
