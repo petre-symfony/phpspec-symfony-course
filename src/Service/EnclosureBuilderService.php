5ed0b0c00538e0ef24380031a424de51ff7e2b71
petrero <petrero@gmail.com>
2019-02-26 18:25:02 +0200

    Copy code from previous branch

---
+++ src/Service/EnclosureBuilderService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Entity\Enclosure;
+use App\Entity\Security;
+use App\Factory\DinosaurFactory;
+
+class EnclosureBuilderService {
+
+	/**
+	 * @var DinosaurFactory
+	 */
+	private $dinosaurFactory;
+
+	public function __construct(DinosaurFactory $dinosaurFactory) {
+		$this->dinosaurFactory = $dinosaurFactory;
+	}
+
+	public function buildEnclosure(
+			int $numberOfSecuritySystems = 1,
+			int $numberOfDinosaurs = 3
+	): Enclosure {
+		$enclosure = new Enclosure();
+
+		$this->addSecuritySystems($numberOfSecuritySystems, $enclosure);
+		$this->addDinosaurs($numberOfDinosaurs, $enclosure);
+
+		return $enclosure;
+	}
+
+	private function addSecuritySystems(
+		int $numberOfSecuritySystems,
+		Enclosure $enclosure
+	){
+		for($i=0; $i<$numberOfSecuritySystems; $i++){
+			$securityName = array_rand(['Fence', 'Electric fence', 'Guard tower']);
+			$security = new Security($securityName, true, $enclosure);
+
+			$enclosure->addSecurity($security);
+		}
+	}
+
+	private function addDinosaurs(int $numberOfDinosaurs, Enclosure $enclosure) {
+		for($i=0; $i<$numberOfDinosaurs; $i++){
+			$enclosure->addDinosaur(
+				$this->dinosaurFactory->growVelociraptor(5 + $i)
+			);
+		}
+	}
+
+
+}
